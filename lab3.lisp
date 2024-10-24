58336e4de634e9326c3fea31e50239f3c7a6a21a
FLeD-jk <122401625+FLeD-jk@users.noreply.github.com>
2024-10-24 22:37:09 +0300

    Update lab3.lisp

--- lab3.lisp
+++ lab3.lisp
@@ -20,6 +20,23 @@
   (let ((n (length lst)))
     (shell-sort-rec lst n (floor (/ n 2)) 0))
 
+(defun check-shell-sorting-functional (name input expected)
+  "Execute shell-sorting-functional on input, compare result with expected and print comparison status"
+  (let ((result (shell-sorting-functional input))) 
+    (format t "~:[~a failed! Expected: ~a Obtained: ~a~;~a passed! Expected: ~a Obtained: ~a~]~%"
+            (equal result expected)
+            name expected result)))
+
+
+(defun test-shell-sorting-functional ()
+ (format t "Start testing shell-sorting-functional function~%")
+(check-shell-sorting-functional "test 1" '(346 23 0 32 44 76 2 120 34  32 65) '(0 2 23 32 32 34 44 65 76 120 346))
+(check-shell-sorting-functional "test 2" '(0 0 2 56 78 21 34 90 6751 1 1 1 -1 1) '(-1 0 0 1 1 1 1 2 21 34 56 78 90 6751))
+(check-shell-sorting-functional "test 3" '(3 4 2 9 34) '(9 2 3 4 34))
+                       (format t "EnD~%"))
+
+  
+(test-shell-sorting-functional)
   
 (defun generate-gaps (n)
   (if (< n 1)
